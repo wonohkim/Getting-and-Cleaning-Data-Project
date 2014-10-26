be79323addac55ec3ad9db4cea1a896ca134027d
Wonoh Kim <wonoh.kim@flukenetworks.com>
2014-10-26 19:53:08 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,6 +7,7 @@
 # body_gyro_z_test = read.table("body_gyro_z_test.txt")
 X_train = read.table("X_train.txt")
 y_train = read.table("y_train.txt")
+subject_train = read.table("subject_train.txt")
 
 # read test data
 # body_acc_x_train = read.table("body_acc_x_train.txt")
@@ -17,6 +18,7 @@ y_train = read.table("y_train.txt")
 # body_gyro_z_train = read.table("body_gyro_z_train.txt")
 X_test = read.table("X_test.txt")
 y_test = read.table("y_test.txt")
+subject_test = read.table("subject_test.txt")
 
 # merge data
 merge_x = rbind(X_train, X_test)
@@ -24,6 +26,7 @@ merge_y = rbind(y_train, y_test)
 
 # tidy data
 tidy_data <- cbind(merge_x, merge_y)
+features <- read.table("features.txt")
 
 # write table
 write.table(tidy_data, file = "./tidy_data.txt")
